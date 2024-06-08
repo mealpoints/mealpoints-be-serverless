8f3b9bfff47e4632206272d6c7c16c9ebe9aa921
scrumtacular-tech <149945037+scrumtacular-tech@users.noreply.github.com>
2024-06-08 21:33:28 +0200

    added more tests

---
+++ test/mocks/user.mock.ts
@@ -0,0 +1,7 @@
+export const USER = {
+  id: "666218de1ae49301e06af692",
+  contact: "917022629939",
+  isActive: true,
+  createdAt: new Date("2024-06-06T22:15:26.2626+02:00"),
+  updatedAt: new Date("2024-06-06T22:15:26.2626+02:00"),
+};
--- test/src/lib/whatsapp/imageMessage.test.ts
+++ test/src/lib/whatsapp/imageMessage.test.ts
@@ -3,6 +3,7 @@ import * as whatsappHandler from "../../../../src/handlers/whatsapp.handler";
 import { processImageMessage } from "../../../../src/lib/whatsapp/imageMessage";
 import * as messageService from "../../../../src/services/message.service";
 import { MessageTypesEnum } from "../../../../src/types/enums";
+import { USER } from "../../../mocks/user.mock";
 import { IMAGE_MESSAGE_PAYLOAD } from "../../../mocks/whatsapp/imageMessage.mock";
 
 jest.mock("../../../../src/handlers/aws.handler");
@@ -15,38 +16,34 @@ describe("processImageMessage", () => {
   });
 
   test("should process image message", async () => {
-    const user = {
-      id: "test-user-id",
-      contact: "test-phone-number",
-      isActive: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
     const conversation = { id: "test-conversation-id" };
 
-    whatsappHandler.getImageSentViaMessage.mockResolvedValue(
+    (whatsappHandler.getImageSentViaMessage as jest.Mock).mockResolvedValue(
       "test-image-file-path"
     );
-    awsHandler.uploadImageToS3.mockResolvedValue("test-s3-path");
-    messageService.updateRecievedMessage.mockResolvedValue({});
-    messageService.sendMessage.mockResolvedValue({});
+    (awsHandler.uploadImageToS3 as jest.Mock).mockResolvedValue("test-s3-path");
+    (messageService.updateRecievedMessage as jest.Mock).mockResolvedValue({});
+    (messageService.sendMessage as jest.Mock).mockResolvedValue({});
 
     // @ts-expect-error - we don't need to pass all the properties of the user object
-    await processImageMessage(IMAGE_MESSAGE_PAYLOAD, user, conversation);
+    await processImageMessage(IMAGE_MESSAGE_PAYLOAD, USER, conversation);
 
     expect(whatsappHandler.getImageSentViaMessage).toHaveBeenCalledWith(
-      "test-image-id"
+      "345150251694683"
     );
     expect(awsHandler.uploadImageToS3).toHaveBeenCalledWith(
-      "test-user-id/test-image-id",
+      `${USER.id}/345150251694683`,
       "test-image-file-path"
     );
     expect(messageService.updateRecievedMessage).toHaveBeenCalledWith(
-      { wamid: "test-message-id" },
+      {
+        wamid:
+          "wamid.HBgMOTE3MDIyNjI5OTM5FQIAEhgWM0VCMDEzODA1QUUwRTQ0ODFENUIzNwA=",
+      },
       { media: "test-s3-path" }
     );
     expect(messageService.sendMessage).toHaveBeenCalledWith({
-      user: "test-user-id",
+      user: USER.id,
       conversation: "test-conversation-id",
       payload: `This is stuff about the image message.`,
       type: MessageTypesEnum.Text,
@@ -54,17 +51,16 @@ describe("processImageMessage", () => {
   });
 
   test("should throw error when processing fails", async () => {
-    const payload = { entry: [] };
     const user = { id: "test-user-id" };
     const conversation = { id: "test-conversation-id" };
 
-    whatsappHandler.getImageSentViaMessage.mockRejectedValue(
+    (whatsappHandler.getImageSentViaMessage as jest.Mock).mockRejectedValue(
       new Error("Test error")
     );
 
     await expect(
       // @ts-expect-error - We are intentionally passing an empty payload
-      processImageMessage(payload, user, conversation)
+      processImageMessage(IMAGE_MESSAGE_PAYLOAD, user, conversation)
     ).rejects.toThrow("Test error");
   });
 });
