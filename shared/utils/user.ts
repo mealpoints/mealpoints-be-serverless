570a03a13f3b488f29f73f94a2094dae4fd90bbc
scrumtacular-tech <149945037+scrumtacular-tech@users.noreply.github.com>
2024-11-23 13:32:42 +0100

    Sending country name

--- shared/utils/user.ts
+++ shared/utils/user.ts
@@ -1,5 +1,6 @@
 import logger from "../config/logger";
 import { IUser } from "../models/user.model";
+import { CountryCodeToNameEnum } from "../types/enums";
 import { getTimeInTimezone } from "./timezone";
 const Logger = logger("shared/utils/user");
 
@@ -9,10 +10,20 @@ export const getUserLocalTime = (user: IUser): Date => {
   return getTimeInTimezone(new Date(), timezone);
 };
 
+const timeAndLocationOfUser = (user: IUser) => {
+  const countryName =
+    CountryCodeToNameEnum[
+      user.countryCode as keyof typeof CountryCodeToNameEnum
+    ];
+
+  return `The user is from ${countryName}, and it's ${new Date().toLocaleString(
+    "en-GB",
+    {
+      timeZone: user.timezone,
+    }
+  )} in ${countryName} right now.`;
+};
+
 export const getInstructionForUser = (user: IUser): string => {
-  return `The user is from ${
-    user.countryCode
-  }, and it's ${new Date().toLocaleString("en-GB", {
-    timeZone: user.timezone,
-  })} there.`;
+  return timeAndLocationOfUser(user);
 };
