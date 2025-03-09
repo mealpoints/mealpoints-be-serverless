10b644b9724f882d762e28586c1d90a61d68e1fa
NeeRaj-2401 <niraj.yadav@slrtce.in>
2025-03-09 21:53:09 +0530

    refactor: update tests and tweak approach

--- shared/libs/subscription/utils.ts
+++ shared/libs/subscription/utils.ts
@@ -37,7 +37,11 @@ export const getSubscriptionStartAndEndDates = (
   };
 };
 
-export const getMaxPossibleBillingCycleCountInPlan = (plan: IPlan): number => {
+// NOTE: user is renewing the subs at this stage, hence we are to return 1 as max possible billing cycle in case of failures
+export const getMaxPossibleBillingCycleCountInPlan = (
+  plan: IPlan,
+  planStartDate: Date
+): number => {
   Logger("getMaxPossibleBillingCycleCountInPlan").info(JSON.stringify(plan));
   const { duration, billingCycle } = plan;
 
@@ -48,18 +52,18 @@ export const getMaxPossibleBillingCycleCountInPlan = (plan: IPlan): number => {
     return 1;
   }
 
+  // for matching units return cycles directly
   if (duration.unit === billingCycle.unit) {
     const cycles = Math.floor(duration.value / billingCycle.value);
     return cycles > 0 ? cycles : 1;
   }
 
-  const referenceDate = new Date();
+  // converting duration and billing cycle into days for easy comparison
+  const planEndDate = addDuration(planStartDate, duration);
+  const billingCycleEndDate = addDuration(planStartDate, billingCycle);
 
-  const planEndDate = addDuration(referenceDate, duration);
-  const billingCycleEndDate = addDuration(referenceDate, billingCycle);
-
-  const planDurationDays = differenceInDays(planEndDate, referenceDate);
-  const billingCycleDays = differenceInDays(billingCycleEndDate, referenceDate);
+  const planDurationDays = differenceInDays(planEndDate, planStartDate);
+  const billingCycleDays = differenceInDays(billingCycleEndDate, planStartDate);
 
   if (billingCycleDays <= 0) {
     Logger("getMaxPossibleBillingCycleCountInPlan").info(
